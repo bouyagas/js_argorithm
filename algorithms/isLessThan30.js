563260163dc467707db80253a3d623ff8efde531
mohamed gassama <bouyagassama@gmail.com>
2017-11-14 18:12:55 -0500

    more

--- algorithms/isLessThan30.js
+++ algorithms/isLessThan30.js
@@ -1,5 +1,8 @@
 const isLessThan30 = (num) => {
-
+ return num < 30;
 };
+
+const output =  isLessThan30.bind(this);
+output.call(this, 9);
 
 module.exports = { isLessThan30 };
--- test/isLessThan30.spec.js
+++ test/isLessThan30.spec.js
@@ -3,7 +3,20 @@ const { isLessThan30 } = require('../algorithms/isLessThan30');
 
 
 describe('isLessThan30', () => {
+
   it('should return a type of a boolean', () => {
     expect(typeof isLessThan30(10)).to.equal('boolean');
   });
-})
+
+  it('should return true if the number is less than 30 ', () => {
+    expect(isLessThan30(4)).to.be.true;
+  });
+
+  it('should return false if the number is greater than 30', () => {
+    expect(isLessThan30(400)).to.be.false;
+  });
+
+  it('should return false if the number is equal to 30', () => {
+    expect(isLessThan30(30)).to.be.false;
+  });
+});
